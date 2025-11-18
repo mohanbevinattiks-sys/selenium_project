7e26aa46f49a4d3e1896790e958b97c052577b21
mohanbevinattikssys <mohanbevinattiks@gmail.com>
2025-11-18 19:51:03 +0530

    adding the method

--- git_selenium/git_learning.py
+++ git_selenium/git_learning.py
@@ -1,6 +1,9 @@
-if 2 == 2:
-    print("correct")
-else:
-    print("not correct")
 
-    
+def even_odd(start, end)
+    for ele in range(start, end)
+        if 2 == 2:
+            print("correct")
+        else:
+            print("not correct")
+
+num = even_odd(10, 20)
